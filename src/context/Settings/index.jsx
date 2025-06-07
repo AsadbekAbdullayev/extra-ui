462bfca65ef3ebbb1c80715c14c560065bc2331e
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-06-07 15:17:24 +0500

    Update index.jsx

--- src/context/Settings/index.jsx
+++ src/context/Settings/index.jsx
@@ -3,6 +3,7 @@ import { reducer, initialState } from './reducer';
 export const Settings = createContext();
 export const useSettings = () => useContext(Settings);
 
+
 export const SettingsProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
