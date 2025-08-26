c9121a5cf9579801941dc485f0405b0c4034659b
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-08-26 10:18:49 +0500

    Update index.jsx

--- src/context/card/index.jsx
+++ src/context/card/index.jsx
@@ -3,14 +3,6 @@ import { reducer, initialState } from './reducer';
 export const ZoneContext = createContext();
 export const useZoneContext = () => useContext(ZoneContext);
 
-
-
-
-
-
-
-
-
 export const ZoneContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
